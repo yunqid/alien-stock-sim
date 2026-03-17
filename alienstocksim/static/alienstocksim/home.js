f605d5fc2d5273184c372ad33ed8ee7be654ad01
Leyud92 <leyud@andrew.cmu.edu>
2026-03-17 04:00:44 -0400

    Made it so that the chart has a max # of datapoints it can store.

--- alienstocksim/static/alienstocksim/home.js
+++ alienstocksim/static/alienstocksim/home.js
@@ -19,8 +19,15 @@ socket.onmessage = function(e) {
     const dataPoint = JSON.parse(e.data);
     const time = new Date().toLocaleTimeString();
 
+    const max_points = 10;
+
     chart.data.labels.push(time);
     chart.data.datasets[0].data.push(dataPoint.price);
+
+    if (chart.data.labels.length > max_points) {
+        chart.data.labels.shift();
+        chart.data.datasets[0].data.shift();
+    }
 
     chart.update();
 };
