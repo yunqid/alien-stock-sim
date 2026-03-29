448967ae5104a28f2272cffafad1009c5f6d3baf
Davis Germain <davisgermain621@gmail.com>
2026-03-29 10:48:05 -0400

    fixing asgi.py to include django.setup()

--- webapps/asgi.py
+++ webapps/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import alienstocksim.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps.settings')
 
 # Tells the channels what to do based on connection
 application = ProtocolTypeRouter({
