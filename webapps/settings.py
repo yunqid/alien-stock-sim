2824d426bcd297d3dd18c562e32fc1bc076710c7
Davis Germain <davisgermain621@gmail.com>
2026-03-29 23:48:10 -0400

    settings changes to have mysql db for production

--- webapps/settings.py
+++ webapps/settings.py
@@ -89,12 +89,23 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DJANGO_ENV') == 'production':
+    DATABASES = {
+        'default': {
+            'OPTIONS': {'charset': 'utf8mb4'},
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'django',
+            'USER': '',
+            'PASSWORD': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
